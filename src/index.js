8c855e56e11dbdd6f62f644d348817cd90a4cffd
CesarTheAwesome <Leonlife528@gmail.com>
2023-12-31 00:03:38 -0500

    better file management and scroll feture buuilt

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import CardList from './CardList.js';
-import App from './App.js';
+import App from './containers/App.js';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
